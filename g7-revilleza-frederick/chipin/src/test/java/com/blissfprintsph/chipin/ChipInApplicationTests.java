9f03a276ef7a44214a7c509a90c7584ef7f68f06
juantojuan <blissfuljuan1989@gmail.com>
2025-03-19 09:58:33 +0800

    Initial code for chipin backend

---
+++ g7-revilleza-frederick/chipin/src/test/java/com/blissfprintsph/chipin/ChipInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blissfprintsph.chipin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
