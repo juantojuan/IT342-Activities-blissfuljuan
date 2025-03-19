13f2df200f6f31d25dc7c722645815df371ce35f
juantojuan <blissfuljuan1989@gmail.com>
2025-03-19 10:09:01 +0800

    configure Spring Security

---
+++ g7-revilleza-frederick/chipin/src/main/java/com/blissfprintsph/chipin/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.blissfprintsph.chipin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Security;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(new JwtAuthenticationConverter())))
+                .build();
+    }
+}
